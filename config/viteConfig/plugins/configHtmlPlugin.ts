7bfd6d355df59eab9dbba94adcb5127195d37c3f
slothman <slothman123@163.com>
2021-11-16 18:17:48 +0800

    1

--- config/viteConfig/plugins/configHtmlPlugin.ts
+++ config/viteConfig/plugins/configHtmlPlugin.ts
@@ -1,39 +1,39 @@
-// /**
-//  * Plugin to minimize and use ejs template syntax in index.html.
-//  * https://github.com/anncwb/vite-plugin-html
-//  */
-//  import type { Plugin } from 'vite';
+/**
+ * 针对 index.html，提供压缩和基于 ejs 模板功能
+ */
+import type { Plugin } from 'vite';
 
-//  import html from 'vite-plugin-html';
+import html from 'vite-plugin-html';
 
-//  import pkg from '../../package.json';
+// import pkg from '../../../package.json';
 
-//  export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
-//    const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
+export default function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
+  const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
 
-//    const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
+  const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
 
-//    const getAppConfigSrc = () => {
-//      return `${path || '/'}_app.config.js?v=${pkg.version}-${new Date().getTime()}`;
-//    };
+  // const getAppConfigSrc = () => {
+  //     return `${path || '/'}_app.config.js?v=${pkg.version}-${new Date().getTime()}`;
+  // };
 
-//    const htmlPlugin: Plugin[] = html({
-//      minify: isBuild,
-//      inject: {
-//        // Inject data into ejs template
-//        injectData: {
-//          title: VITE_GLOB_APP_TITLE,
-//        },
-//        // Embed the generated app.config.js file
-//        tags: isBuild?[
-//             {
-//             tag: 'script',
-//             attrs: {
-//               src: getAppConfigSrc(),
-//             },
-//           },
-//         ]:[]
-//      },
-//    });
-//    return htmlPlugin;
-//  }
+  const htmlPlugin: Plugin[] = html({
+    minify: isBuild,
+    inject: {
+      //将数据注入ejs模板
+      injectData: {
+        title: VITE_GLOB_APP_TITLE,
+      },
+      //  标签注入到HTML中  默认位置是 head 前
+      // tags: isBuild ? [
+      //     {
+      //         injectTo:"body",
+      //         tag: 'script',
+      //         attrs: {
+      //             src: getAppConfigSrc(),
+      //         },
+      //     },
+      // ] : []
+    },
+  });
+  return htmlPlugin;
+}
--- config/viteConfig/plugins/configSvgIconsPlugin.ts
+++ config/viteConfig/plugins/configSvgIconsPlugin.ts
@@ -15,3 +15,19 @@
 //   });
 //   return svgIconsPlugin;
 // }
+/**
+ *  svg 处理
+ */
+import type { Plugin } from 'vite';
+import SvgIconsPlugin from 'vite-plugin-svg-icons';
+import path from 'path';
+
+export default function configSvgIconsPlugin(isBuild: boolean): Plugin {
+  const svgIconsPlugin: Plugin = SvgIconsPlugin({
+    iconDirs: [path.resolve(process.cwd(), 'public/assets/icons/svg')],
+    svgoOptions: isBuild,
+    // 指定symbolId格式
+    symbolId: 'icon-[dir]-[name]',
+  });
+  return svgIconsPlugin;
+}
