51f6a88ee134b526bede22ce6f9b854a268a5bf0
slothman <slothman123@163.com>
2021-12-15 14:46:04 +0800

    .

--- client/views/uploadfile.vue
+++ client/views/uploadfile.vue
@@ -55,7 +55,7 @@
           url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
         },
       ];
-      //
+      //图片调整后触发
       const changelist = (arg: any[]) => {
         fileList = arg;
         console.log(fileList);
